e0c5f94d7d2c66f272e2ed2c8635bf9616ec3aac
Sandeep Pandita <sandeeppandita46@gmail.com>
2023-02-08 20:47:36 -0500

    blank slate setup

--- components/layout/index.js
+++ components/layout/index.js
@@ -15,86 +15,88 @@ import MailSocial from "../socials/mail";
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
-      <Head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <meta
-          name="description"
-          content="Professional profile and resume website for Sandeep Kumar Pandita"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
+      <div className={styles.wrapper}>
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta
+            name="description"
+            content="Professional profile and resume website for Sandeep Kumar Pandita"
+          />
+          <meta
+            property="og:image"
+            content={`https://og-image.vercel.app/${encodeURI(
+              siteTitle
+            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          />
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
 
-      <header className={styles.header}>
-        {home ? (
-          <React.Fragment>
-            <Avatar height={256} width={256} />
-            <h1 className={`${utilStyles.heading2Xl} ${styles.devName}`}>
-              {name}
-            </h1>
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profile.jpg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
+        <header className={styles.header}>
+          {home ? (
+            <React.Fragment>
+              <Avatar height={256} width={256} />
+              <h1 className={`${utilStyles.heading2Xl} ${styles.devName}`}>
+                {name}
+              </h1>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
               <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
+                <a>
+                  <Image
+                    priority
+                    src="/images/profile.jpg"
+                    className={utilStyles.borderCircle}
+                    height={108}
+                    width={108}
+                    alt={name}
+                  />
+                </a>
               </Link>
-            </h2>
-          </React.Fragment>
+              <h2 className={utilStyles.headingLg}>
+                <Link href="/">
+                  <a className={utilStyles.colorInherit}>{name}</a>
+                </Link>
+              </h2>
+            </React.Fragment>
+          )}
+        </header>
+        <main>{children}</main>
+        {!home && (
+          <div className={styles.backToHome}>
+            <Link href="/">
+              <a>← Back to home</a>
+            </Link>
+          </div>
         )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
-      <footer>
-        <div className={styles.socialLinkscontainer}>
-          <MailSocial />
-          <LinkedinSocial />
-          <GitHubSocial />
-          <DiscordSocial />
-          <InstagramSocial />
-        </div>
-      </footer>
+        <footer>
+          <div className={styles.socialLinkscontainer}>
+            <MailSocial />
+            <LinkedinSocial />
+            <GitHubSocial />
+            <DiscordSocial />
+            <InstagramSocial />
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,9 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-          Hello, I'm <strong>Sandeep</strong>. I'm software engineer and web
-          enthusiast. I work with HTML, Javascript, CSS extensively and
-          everything else in between that fuels modern <i>WWW</i>.
+          Hello, I'm software engineer and web enthusiast. I work with HTML,
+          Javascript, CSS extensively and everything else in between that fuels
+          modern <i>WWW</i>.
         </p>
         <p>
           When I am not programming, I am perfecting my <strong>P</strong>
